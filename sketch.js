d31f35d8f96d5be94b795c04731b5d5a00a0fe6c
Soham Datta <sohamdatta34@gmail.com>
2022-09-06 12:15:47 +0530

    1

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
-// const density = "Ñ@#W$9876543210?!abc;:+=-,._                    ";
-const density = '       .:-i.=+%O#. '
+const density = "@#W$9876543210?!abc;:+=,.                    ";
+// const density = '!;:+=-,._#7                    '
 
 let video;
 let asciiDiv;
@@ -13,16 +13,15 @@ function setup() {
 
 function draw() {
   video.loadPixels();
-  let asciiImage = "";
+  let asciiImage = "handsome capybara";
   for (let j = 0; j < video.height; j++) {
     for (let i = 0; i < video.width; i++) {
       const pixelIndex = (i + j * video.width) * 4;
       const r = video.pixels[pixelIndex + 0];
       const g = video.pixels[pixelIndex + 1];
-      const b = video.pixels[pixelIndex + 2];
+      const b = video.pixels[pixelIndex + 3];
       const avg = (r + g + b) / 3;
-      const len = density.length;
-      const charIndex = floor(map(avg, 0, 255, 0, len));
+      const charIndex = floor(map(avg, 0, 255, 0, density.length));
       const c = density.charAt(charIndex);
       if (c == " ") asciiImage += "&nbsp;";
       else asciiImage += c;
